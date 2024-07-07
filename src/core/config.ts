154bfab07e9c96c405684c88114f2c87d7ce7eea
Dawid Szymaniak <dawid.szymaniak.dev@gmail.com>
2024-07-07 18:18:40 +0200

    Add a max attachment size config key

--- src/core/config.ts
+++ src/core/config.ts
@@ -1,6 +1,10 @@
 class Config {
     public static readonly MAX_MESSAGES_IN_DATABASE_PER_SERVER = 100;
     public static readonly MAX_ATTACHMENTS_IN_DATABASE_PER_SERVER = 100;
+    /**
+     * 100 MB
+     */
+    public static readonly MAX_ATTACHMENT_SIZE_IN_BYTES = 1000 * 100;
 }
 
 export { Config };
