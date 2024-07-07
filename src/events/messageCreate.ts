3ca1e1f40a6fbf61f325bcd8eb78641e180d7241
Dawid Szymaniak <dawid.szymaniak.dev@gmail.com>
2024-07-07 19:11:14 +0200

    Update messageCreate handler to handle attachments
    Update the messageCreate handler to upload attachments to Google Cloud and save their metadata to the database

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,9 +1,15 @@
+import dotenv from 'dotenv';
+import { v4 } from 'uuid';
 import { Events, ClientEvents } from 'discord.js';
+import { Config } from '@/core/config';
 import { InsertMessage } from '@/database/schema';
-import { insertMessage } from '@/database/utilities';
+import { insertMessage, insertAttachment } from '@/database/utilities';
+import { saveAttachmentFromUrl } from '@/gcloud/utilities';
+
+dotenv.config();
 
 const event = Events.MessageCreate;
-const handler = (...[message]: ClientEvents[typeof event]) => {
+const handler = async (...[message]: ClientEvents[typeof event]) => {
     if (
         !message.guild ||
         message.author.id === message.client.user.id ||
@@ -23,7 +29,27 @@ const handler = (...[message]: ClientEvents[typeof event]) => {
         dateEdited: message.editedAt,
     };
 
-    insertMessage(messageMetadata);
+    const { message: insertedMessage } = await insertMessage(messageMetadata);
+
+    if (message.attachments.size > 0) {
+        message.attachments.forEach(async (attachment) => {
+            try {
+                const destinationFileName = `${attachment.id}_${v4()}_${attachment.name}`;
+                saveAttachmentFromUrl(
+                    Config.GOOGLE_CLOUD_STORAGE_BUCKET,
+                    attachment.url,
+                    destinationFileName,
+                );
+                insertAttachment(insertedMessage, {
+                    fileUrl: `https://storage.googleapis.com/${Config.GOOGLE_CLOUD_STORAGE_BUCKET}/${destinationFileName}`,
+                    messageId: insertedMessage.messageId,
+                });
+            } catch (error) {
+                /* TODO: If there is an error saving the attachment, revert any additions to gcloud and the database */
+                console.error(error);
+            }
+        });
+    }
 };
 
 export { event, handler };
