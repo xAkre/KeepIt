e28b93375ffc00fbe872d0fe8cdbb663c483403a
Dawid Szymaniak <dawid.szymaniak.dev@gmail.com>
2024-07-07 17:26:26 +0200

    Add a Config class which contains non .env configuration

---
+++ src/core/config.ts
@@ -0,0 +1,6 @@
+class Config {
+    public static readonly MAX_MESSAGES_IN_DATABASE_PER_SERVER = 100;
+    public static readonly MAX_ATTACHMENTS_IN_DATABASE_PER_SERVER = 100;
+}
+
+export { Config };
